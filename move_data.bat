9e9edf709dcd443976f677b64a715c00b6dcc057
Evan Ott <evan.ott@utexas.edu>
2014-08-16 21:18:26 -0500

    moved data to Dropbox

---
+++ move_data.bat
@@ -0,0 +1,8 @@
+cd C:\Users\Evan\GitProjects\weather-prediction
+git pull
+cd C:\Users\Evan\GitProjects\weather-prediction\data
+move * C:\Users\Evan\Dropbox\Thesis_Data
+cd ..
+git add --all .
+git commit -m "moved data to Dropbox"
+git push
